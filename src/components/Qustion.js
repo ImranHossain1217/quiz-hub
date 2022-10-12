d2f2cc239bb9006ecfd3e8463ab1fc44333521fe
Imran Hossain Imon <imranhossainemon1717@gmail.com>
2022-10-12 21:39:45 +0600

    toast and quiz answer added

--- src/components/Qustion.js
+++ src/components/Qustion.js
@@ -1,28 +1,51 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Qustion = () => {
-    const qustions = useLoaderData();
-    const quizQustions = qustions.data.questions;
-    return (
-        <div>
-            <h1 className='mt-5 text-3xl'>Start Your Quiz</h1>
-               <div className=''>
-                {
-                    quizQustions.map(qustion => {
-                        return <div className='border w-8/12 m-5 p-3 mx-auto bg-blue-200 rounded'>
-                        <h3 className='md:text-xl mb-2'>Qustion: {qustion.question}</h3>
-                           <div className='grid grid-cols-1 md:grid-cols-2 gap-2 w-2/3 mx-auto'>
-                              {
-                                qustion.options.map(option => <p className='m-2 border p-2 bg-blue-900 text-white rounded-md'>{option}</p>)
-                              }
-                           </div>
-                        </div>
-                    })
-                }
-               </div>
-        </div>
-    );
+  const qustions = useLoaderData();
+  const quizQustions = qustions.data.questions;
+  const handleQuizAnswer = (question, option) => {
+    if (question.correctAnswer === option) {
+      toast.success("Your Answer is Correct!!", {
+        position: "top-center",
+        theme: "colored",
+      });
+    } else {
+      toast.error("Your Answer is Wrong!!", {
+        position: "top-center",
+        theme: "colored",
+      });
+    }
+  };
+  return (
+    <div>
+      <h1 className="mt-5 text-3xl">Start Your Quiz</h1>
+      <div className="">
+        {quizQustions.map((qustion) => {
+          return (
+            <div
+              key={qustion.id}
+              className="border w-8/12 m-5 p-3 mx-auto bg-blue-200 rounded"
+            >
+              <h3 className="md:text-xl mb-2">Qustion: {qustion.question}</h3>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-2 w-2/3 mx-auto">
+                {qustion.options.map((option) => (
+                  <p
+                    onClick={() => handleQuizAnswer(qustion, option)}
+                    className="border p-2 m-2 bg-blue-900 text-white rounded-md cursor-pointer"
+                  >
+                    {option}
+                  </p>
+                ))}
+                <ToastContainer></ToastContainer>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Qustion;
