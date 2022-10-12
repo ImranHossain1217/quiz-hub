47b20996e4df37bd67be124f20ec986a2bf3bc5c
Imran Hossain Imon <imranhossainemon1717@gmail.com>
2022-10-12 20:07:15 +0600

    quiz option added

--- src/components/Qustion.js
+++ src/components/Qustion.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import Option from './Option';
 
 const Qustion = () => {
     const qustions = useLoaderData();
     const quizQustions = qustions.data.questions;
-    console.log(quizQustions);
     return (
         <div>
-            <h1>qustion components: {quizQustions.length}</h1>
+            <h1 className='mt-5 text-3xl'>Start Your Quiz</h1>
                <div className=''>
                 {
                     quizQustions.map(qustion => {
-                        return <div className='border w-8/12 m-5 p-3 mx-auto'>
-                        <h3>Qustion: {qustion.question}</h3>
+                        return <div className='border w-8/12 m-5 p-3 mx-auto bg-blue-200 rounded'>
+                        <h3 className='md:text-xl mb-2'>Qustion: {qustion.question}</h3>
                            <div className='grid grid-cols-1 md:grid-cols-2 gap-2 w-2/3 mx-auto'>
-                            <Option></Option>
+                              {
+                                qustion.options.map(option => <p className='m-2 border p-2 bg-blue-900 text-white rounded-md'>{option}</p>)
+                              }
                            </div>
                         </div>
                     })
