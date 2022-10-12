30c6d1a44a2c1252549fc5536a01a504f31ecbf1
Imran Hossain Imon <imranhossainemon1717@gmail.com>
2022-10-12 23:43:06 +0600

    readme file added and some bug fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,44 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  let activeStyle = {
+    color: "blue",
+  };
   return (
     <div className="flex justify-between p-6">
       <div>
         <h1 className="text-blue-500 text-2xl">Quiz Hub</h1>
       </div>
       <div>
-        <Link className="mr-2 md:mr-5 hover:text-blue-500" to="/home">
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          className="mr-2 md:mr-5 hover:text-blue-500"
+          to="/home"
+        >
           Home
-        </Link>
-        <Link className="mr-2 md:mr-5 hover:text-blue-500" to="/topics">
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          className="mr-2 md:mr-5 hover:text-blue-500"
+          to="/topics"
+        >
           Topics
-        </Link>
-        <Link className="mr-2 md:mr-5 hover:text-blue-500" to="/statics">
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          className="mr-2 md:mr-5 hover:text-blue-500"
+          to="/statics"
+        >
           Statics
-        </Link>
-        <Link className="mr-2 md:mr-5 hover:text-blue-500" to="/blog">
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          className="mr-2 md:mr-5 hover:text-blue-500"
+          to="/blog"
+        >
           Blog
-        </Link>
+        </NavLink>
       </div>
     </div>
   );
--- src/components/Topics.js
+++ src/components/Topics.js
@@ -8,7 +8,7 @@ const Topics = () => {
   return (
     <div>
       <h1 className="text-blue-500 mt-10 text-3xl">Select Your Quiz Topic</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:w-2/4 mx-auto m-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:w-10/12 mx-auto m-10">
         {quizTopics.map((topic) => (
           <QuizTopicCard key={topic.id} topic={topic}></QuizTopicCard>
         ))}
